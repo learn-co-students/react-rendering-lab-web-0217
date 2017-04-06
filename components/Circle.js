a20d565ca6bf6ec4dba02f5b68b64c4599ccf6b3
github username <github email address>
2017-04-05 20:39:40 -0400

    Done.

--- components/Circle.js
+++ components/Circle.js
@@ -2,6 +2,9 @@ const React = require('react');
 
 class Circle extends React.Component {
 
+  shouldComponentUpdate(nextProps, nextState) {
+    (this.props.color !== nextProps.color ? true : false)
+  }
   render() {
     const { color } = this.props;
     console.log(`A ${color} circle was rendered!`);
